b5cd576b545526bec88a7141735c88331337c12e
Rafael Ramirez <rafael.jr.ramirez@outlook.com>
2021-04-11 14:16:17 -0700

    Added Edit Workout

--- WebApplication2/Controllers/HomeController.cs
+++ WebApplication2/Controllers/HomeController.cs
@@ -38,6 +38,11 @@ namespace WebApplication2.Controllers
             return View();
         }
 
+        public IActionResult EditWorkout()
+        {
+            return View();
+        }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
