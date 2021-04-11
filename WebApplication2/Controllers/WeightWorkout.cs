e8b3104fc4112fac2f176232d1aa55a8aa4b4c85
Rafael Ramirez <rafael.jr.ramirez@outlook.com>
2021-04-11 13:59:55 -0700

    Initial Commit

---
+++ WebApplication2/Controllers/WeightWorkout.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Nxt_Level_Fitness.Models;
+
+
+namespace Nxt_Level_Fitness.Controllers
+{
+    public class WeightWorkoutController : Controller
+    {
+        // M e t h o d s
+
+        // C r e a t e
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(WeightWorkout w)
+        {
+            w.Id = 1;
+            w.UserId = 1;
+
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("WeightWorkoutDetails", w);
+            }
+
+            return RedirectToAction("WeightWorkoutDetails", w);
+            //return View(w);
+        }
+
+
+        [HttpGet]
+        public IActionResult EditWeightWorkout(int id)
+        {
+            // fake information
+            WeightWorkout ww = new WeightWorkout();
+            ww.Id = id;
+            ww.WeightWorkouts = "Chest";
+            ww.Calories = 600;
+            return View(ww);
+        }
+
+        [HttpPost]
+        public IActionResult EditWeightWorkout(WeightWorkout updatedWeightWorkout)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("WeightWorkoutDetails", updatedWeightWorkout);
+            }
+
+            return View(updatedWeightWorkout);
+        }
+
+        public IActionResult WeightWorkoutDetails(WeightWorkout w)
+        {
+            return View(w);
+        }
+        // R e a d
+
+        // U p d a t e
+
+        // D e l e t e
+    }
+}
---
+++ WebApplication2/Models/WeightWorkout.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Nxt_Level_Fitness.Models
+{
+    public class WeightWorkout
+    {
+        // F I E L D S & P R O P E R T I E S
+
+        // [Display(Name = "Id")]
+        // [MaxLength(40, ErrorMessage = "ID is Limited to 40 Characters")]
+        // [Required(ErrorMessage = "ID Is Required")]
+        public int Id { get; set; }
+
+        //[Display(Name = "WeightWorkout")]
+        [MaxLength(40, ErrorMessage = "Weight Workout is Limited to 40 Characters")]
+        [Required(ErrorMessage = "Weight Workout Is Required")]
+        public string WeightWorkouts { get; set; }
+
+        //[Column(TypeName = "Date")]
+        //[DataType(DataType.Date)]
+        //[Display(Name ="Date")]
+        //[UIHint("Date")]
+        public string Date { get; set; }
+
+        //[Display(Name = "Calories")]
+        [Range(0, 5000, ErrorMessage = "Please enter a valid number")]
+        [Column]
+        [Required(ErrorMessage = "Calories Is Required")]
+        public int Calories { get; set; }
+
+        //[HiddenInput(DisplayValue = false)]
+        public int UserId { get; set; }
+
+        // C O N S T R U C T O R S
+
+
+
+        // M E T H O D S
+
+
+
+    }
+}
